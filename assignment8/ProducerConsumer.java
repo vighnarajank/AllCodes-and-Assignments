53e699cbe61fd76941a24af25c52af2278ea619f
Vighna Rajan K <84614987+vighnarajank@users.noreply.github.com>
2021-05-31 00:20:58 +0530

    Update ProducerConsumer.java

--- assignment8/ProducerConsumer.java
+++ assignment8/ProducerConsumer.java
@@ -1,101 +1,95 @@
 package com.onebill.corejava.assignment8;
 
-import java.util.Scanner;
-
-class Storage {
-	int MAX_CAPACITY = 100;
-	int capacity;
-
-	void toCalculate(int type, int no_of_items) {
-		if (type == 1) {
-			if((capacity+no_of_items)>MAX_CAPACITY) {
-				System.err.println("Storage filled....! Try giving less items");
+class storage{
+	int product;
+	boolean bool = false;
+	
+	public synchronized void setProduct(int product) {
+		while(bool) {
+			try {
+				System.out.println("Set waiting.....");
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			else {
-				capacity += no_of_items;
-				System.out.println("Storage  : "+capacity);
-			}
-
-
-
-		} else {
-			if((capacity-no_of_items)<0) {
-				System.err.println("Empty storage....!");
-			}
-			else {
-				capacity -= no_of_items;
-				System.out.println("Storage  : "+capacity);
-			}
-
 		}
-
+		System.out.println("Set : " + product);
+		this.product = product;
+		bool=true;
+		notify();
 	}
+	
+	public synchronized void getProduct() {
+		while(!bool) {
+			try {
+				System.out.println("Get waiting.....");
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Get : " + this.product);
+		bool=false;
+		notify();
+	}
+
 }
 
-class Producer extends Thread {
-	int produce_no_of_items;
-	Storage p_storage;
+class producer implements Runnable{
 
-	public Producer(Storage obj) {
-		p_storage = obj;
+	storage S;
+	producer(storage st){
+		this.S = st;
 	}
-
-	void input(Scanner sc) {
-		System.out.println("Enter the no. of items to be produce : ");
-		produce_no_of_items = sc.nextInt();
-	}
-
+	
 	@Override
 	public void run() {
-
-		p_storage.toCalculate(1, produce_no_of_items);
+		int i=0;
+		while(true) {
+			S.setProduct(i++);
+			try {
+				Thread.sleep(900);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
+
 }
-class Consumer extends Thread {
-	int consumer_no_of_items;
-	Storage c_storage;
 
-	public Consumer(Storage obj) {
-		c_storage = obj;
+class consumer implements Runnable{
+
+	storage S;
+	consumer(storage st){
+		this.S = st;
 	}
-
-	void input(Scanner sc) {
-		System.out.println("Enter the no. of items to be consume : ");
-		consumer_no_of_items = sc.nextInt();
-	}
-
+	
 	@Override
 	public void run() {
-
-		c_storage.toCalculate(2, consumer_no_of_items);
+		while(true) {
+			S.getProduct();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
+
 }
 
 public class ProducerConsumer {
 
 	public static void main(String[] args) {
-		int ch,exit=1;
-		Scanner sc = new Scanner(System.in);
-		Storage obj = new Storage();
-		Producer producer = new Producer(obj);
-		Consumer consumer = new Consumer(obj);
-		while(exit==1) {
-			System.out.println(" 1 : Producer\n 2 : Consumer\n 3 :Exit\nEnter the choice : ");
-			ch = sc.nextInt();
-			switch (ch) {
-			case 1: 
-				producer.input(sc);
-				producer.run();
-				break;
-			case 2: 
-				consumer.input(sc);
-				consumer.run();
-				break;
-			case 3 : 
-				exit=0;
-
-			}
-		}
+		storage st = new storage();
+		producer p = new producer(st);
+		consumer c = new consumer(st);
+		Thread t1 = new Thread(p);
+		Thread t2 = new Thread(c);
+		t1.start();
+		t2.start();
 
 	}
 
